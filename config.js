2360d8812e3db22c6fc24357274defec364a24fd
gianger <106813566+gianger@users.noreply.github.com>
2022-06-03 19:26:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,6 +15,6 @@ const CONFIG = {
     reply: 'Vì tớ thích thế',
     mess: 'Buồn ghê thôi vậy',
     messDesc: 'Tạm biệt bạn .',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
