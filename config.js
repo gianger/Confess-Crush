420a9e6b83495ea49a0900448a4415289b2f8dec
gianger <106813566+gianger@users.noreply.github.com>
2022-06-03 19:05:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: " nothing ",
+    introTitle: 'someone',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    title: 'Giờ phải chọn nhé, không chọn là không đuọc làm ny tớ đâu nha',
+    desc: 'làm ny tớ nhé :)))) ',
+    btnYes: 'không nhé :)))',
+    btnNo: 'Có chứ chời',
+    question: 'Ơ tại sao',
+    btnReply: 'Gửi cho bạn ',
+    reply: 'Thích thế đó',
+    mess: 'thế thôi vậy huhu',
+    messDesc: 'Tạm biệt bạn nhé',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
